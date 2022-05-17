9dc832d9b7aa50f1cae4a4ec1bbc27471bac12a3
Asher Norland <asher.norland@gmail.com>
2022-05-17 18:22:50 -0400

    Pass Additional Args to Subprocess

--- cppython_core/utility.py
+++ cppython_core/utility.py
@@ -8,13 +8,15 @@ from pathlib import Path
 cppython_logger = getLogger("cppython")
 
 
-def subprocess_call(arguments: list[str | Path], suppress: bool = False):
+def subprocess_call(arguments: list[str | Path], suppress: bool = False, **kwargs):
     """
     Executes a subprocess call with logger and utility attachments. Captures STDOUT and STDERR
     """
 
     try:
-        process = subprocess.run(arguments, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True, text=True)
+        process = subprocess.run(
+            arguments, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True, text=True, **kwargs
+        )
 
         if not suppress:
             cppython_logger.info(process.stdout)
