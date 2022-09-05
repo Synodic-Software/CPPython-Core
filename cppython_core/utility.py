9273df865f0f69fb3919d3748377ec21b3478fc5
Asher Norland <asher.norland@gmail.com>
2022-09-05 17:46:11 -0500

    Strengthen Subprocess Wrapper (#56)

--- cppython_core/utility.py
+++ cppython_core/utility.py
@@ -1,38 +1,32 @@
 """
 Core Utilities
 """
+
 import logging
 import subprocess
+from logging import Logger
 from pathlib import Path
 from typing import Any
 
-cppython_logger = logging.getLogger("cppython")
+from cppython_core.exceptions import ProcessError
 
 
 def subprocess_call(
-    arguments: list[str | Path], log_level: int = logging.WARNING, suppress: bool = False, **kwargs: Any
+    arguments: list[str | Path], logger: Logger, log_level: int = logging.WARNING, suppress: bool = False, **kwargs: Any
 ):
     """
     Executes a subprocess call with logger and utility attachments. Captures STDOUT and STDERR
     """
 
-    try:
-        process = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, **kwargs)
+    process = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, **kwargs)
 
-        if not suppress:
-            assert process.stdout is not None
-            with process.stdout as pipe:
-                for line in iter(pipe.readline, ""):
-                    cppython_logger.log(log_level, line.rstrip())
+    if not suppress:
+        assert process.stdout is not None
+        with process.stdout as pipe:
+            for line in iter(pipe.readline, ""):
+                logger.log(log_level, line.rstrip())
 
-        exitcode = process.wait()
+    exitcode = process.wait()
 
-        if exitcode != 0:
-            raise subprocess.CalledProcessError(exitcode, arguments)
-
-    except subprocess.CalledProcessError as error:
-
-        if not suppress:
-            cppython_logger.error(f"The process failed with: {error.stdout}")
-
-        raise error
+    if exitcode != 0:
+        raise ProcessError("Subprocess task failed")
