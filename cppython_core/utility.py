b07562b5bbdff9db0401df737fbe1480d9b1c42e
Asher Norland <asher.norland@gmail.com>
2022-09-25 12:44:04 -0400

    Add Writing Utilities

--- cppython_core/utility.py
+++ cppython_core/utility.py
@@ -1,13 +1,17 @@
 """Core Utilities
 """
 
+import json
 import logging
 import subprocess
 from logging import Logger
 from pathlib import Path
 from typing import Any
 
+from pydantic import BaseModel
+
 from cppython_core.exceptions import ProcessError
+from cppython_core.schema import ModelT
 
 
 def subprocess_call(
@@ -37,3 +41,56 @@ def subprocess_call(
 
     if process.returncode != 0:
         raise ProcessError("Subprocess task failed")
+
+
+def read_model_json(path: Path, model: type[ModelT]) -> ModelT:
+    """Reading routine. Only keeps Model data
+
+    Args:
+        path: The file to read
+        model: The model to read
+
+    Returns:
+        The read model
+    """
+
+    return model.parse_file(path=path)
+
+
+def read_json(path: Path) -> Any:
+    """Reading routine
+
+    Args:
+        path: The json file to read
+
+    Returns:
+        The json data
+    """
+
+    with open(path, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def write_model_json(path: Path, model: BaseModel) -> None:
+    """Writing routine. Only writes model data
+
+    Args:
+        path: The json file to write
+        model: The model to write into a json
+    """
+
+    serialized = json.loads(model.json(exclude_none=True))
+    with open(path, "w", encoding="utf8") as file:
+        json.dump(serialized, file, ensure_ascii=False, indent=4)
+
+
+def write_json(path: Path, data: Any) -> None:
+    """Writing routine
+
+    Args:
+        path: The json to write
+        data: The data to write into json
+    """
+
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
