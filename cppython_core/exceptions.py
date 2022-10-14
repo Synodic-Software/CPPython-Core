0e17c1a73613963e1679d9ac45f52efcc299d203
Asher Norland <asher.norland@gmail.com>
2022-10-14 03:30:31 -0500

    PluginError (#100)

--- cppython_core/exceptions.py
+++ cppython_core/exceptions.py
@@ -20,6 +20,24 @@ class ConfigError(Exception):
         return self._error
 
 
+class PluginError(Exception):
+    """Raised when there is a plugin error"""
+
+    def __init__(self, error: str) -> None:
+        self._error = error
+
+        super().__init__(error)
+
+    @property
+    def error(self) -> str:
+        """Returns the underlying error
+
+        Returns:
+            str -- The underlying error
+        """
+        return self._error
+
+
 class ProcessError(Exception):
     """Raised when there is a configuration error"""
 
