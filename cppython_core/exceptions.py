3fbd2c9cd24c90b535d7d5d5807f81215b2d96bc
Asher Norland <asher.norland@gmail.com>
2022-10-31 08:10:36 -0500

    Remove Query, Favoring Exception (#109)

--- cppython_core/exceptions.py
+++ cppython_core/exceptions.py
@@ -38,6 +38,24 @@ class PluginError(Exception):
         return self._error
 
 
+class NotSupportedError(Exception):
+    """Raised when something is not supported"""
+
+    def __init__(self, error: str) -> None:
+        self._error = error
+
+        super().__init__(error)
+
+    @property
+    def error(self) -> str:
+        """Returns the underlying error
+
+        Returns:
+            str -- The underlying error
+        """
+        return self._error
+
+
 class ProcessError(Exception):
     """Raised when there is a configuration error"""
 
