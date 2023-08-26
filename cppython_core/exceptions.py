7b2f6ca8558aa4fdaebff00e6bf45c7070c4a601
Asher Norland <asher.norland@gmail.com>
2023-08-26 12:39:10 -0400

    Move Utilities

--- cppython_core/exceptions.py
+++ cppython_core/exceptions.py
@@ -54,21 +54,3 @@ class NotSupportedError(Exception):
             str -- The underlying error
         """
         return self._error
-
-
-class ProcessError(Exception):
-    """Raised when there is a configuration error"""
-
-    def __init__(self, error: str) -> None:
-        self._error = error
-
-        super().__init__(error)
-
-    @property
-    def error(self) -> str:
-        """Returns the underlying error
-
-        Returns:
-            str -- The underlying error
-        """
-        return self._error
